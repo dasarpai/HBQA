1270e974262ab93a2dd7b93d4cfe36363c622627
Hari Thapliyaal <hari.prasad@vedavit-ps.com>
2023-10-28 19:16:41 +0530

    misc

--- Utilfunc.py
+++ Utilfunc.py
@@ -24,3 +24,30 @@ def select_embmodel(num, verbose=True):
         print (embmodelname,'\t',embmodelshort,'\t', embmodelname1)
         
     return embmodelname, embmodelshort, embmodelname1
+
+def describe(df):
+    import pandas as pd
+    summary = df.describe()
+    sum_row = df.sum().to_frame().T
+    sum_row.index = ['sum']
+    summary = pd.concat([summary, sum_row])
+    return pd.DataFrame(summary)
+
+def select_predmodel(n):
+    predmodels_list = {"0" :"T5:t5", "1" : "Flan-T5:flant5", "2":"GPT2:gpt2","3":"DistilBERT:distilbert",
+                   "4":"RoBERTa:roberta","5":"Llama2:llama2", "6":"BERTSquad:bert", 
+                   "7":"LongFormer:longformer", "8":"DistilBERT512:distilbert512"}
+    predmodel_name = predmodels_list['2'].split(':')[n]
+    return predmodel_name
+
+def convert_to_int(values):
+    v1=[]
+    for v in values:
+        v1.append(int(v))
+    return v1
+
+def convert_to_float(values,n=2):
+    v1=[]
+    for v in values:
+        v1.append( round(float(v),n))
+    return v1
